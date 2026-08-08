81f8d80753cb78bc09a197fb269f58ba60258f3a
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-08-08 20:32:32 +0500

    feat: Created entity for Author and connected MySQL database

--- src/main/java/pk/edu/niit/library_management_system/Author/Entity/Author.java
+++ src/main/java/pk/edu/niit/library_management_system/Author/Entity/Author.java
@@ -1,4 +1,18 @@
 package pk.edu.niit.library_management_system.Author.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Author")
+@Data
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long author_id;
+    @Column
+    private String author_name;
+    @Column
+    private String bio;
+
 }
