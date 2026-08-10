69a1e944e3a49ccd4d0366038f94de2cf2328caa
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-08-10 09:55:07 +0500

    feat: Added Repository and Services for Member

--- src/main/java/pk/edu/niit/library_management_system/Member/Repository/MemberRepository.java
+++ src/main/java/pk/edu/niit/library_management_system/Member/Repository/MemberRepository.java
@@ -1,4 +1,9 @@
 package pk.edu.niit.library_management_system.Member.Repository;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pk.edu.niit.library_management_system.Member.Entity.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
 }
--- src/main/java/pk/edu/niit/library_management_system/Member/Services/MemberServices.java
+++ src/main/java/pk/edu/niit/library_management_system/Member/Services/MemberServices.java
@@ -1,4 +1,47 @@
 package pk.edu.niit.library_management_system.Member.Services;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pk.edu.niit.library_management_system.BorrowRecord.Entity.BorrowRecord;
+import pk.edu.niit.library_management_system.Member.Entity.Member;
+import pk.edu.niit.library_management_system.Member.Repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Data
 public class MemberServices {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public List<Member> getAllMembers()
+    {
+        return memberRepository.findAll();
+    }
+
+    public Member createMember(Member member)
+    {
+        return memberRepository.save(member);
+    }
+
+    public void deleteMember(long id)
+    {
+        memberRepository.deleteById(id);
+    }
+
+    public Member updateMember(long id, Member member)
+    {
+        Optional<Member> existing=memberRepository.findById(id);
+        if(existing.isPresent())
+        {
+            Member updated=existing.get();
+            updated.setMemberName(member.getMemberName());
+            updated.setEmail(member.getEmail());
+            updated.setMembershipDate(member.getMembershipDate());
+            return memberRepository.save(updated);
+        }
+        return null;
+    }
 }
