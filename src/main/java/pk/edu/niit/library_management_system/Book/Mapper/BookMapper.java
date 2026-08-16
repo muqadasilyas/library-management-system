9ad65452c1824f96d6af7940a11c6d14a1ae2d4e
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-08-17 00:00:32 +0500

    feat: Added Mapper for Book

---
+++ src/main/java/pk/edu/niit/library_management_system/Book/Mapper/BookMapper.java
@@ -0,0 +1,14 @@
+package pk.edu.niit.library_management_system.Book.Mapper;
+
+import org.springframework.stereotype.Component;
+import pk.edu.niit.library_management_system.Book.DTO.BookRequestDTO;
+import pk.edu.niit.library_management_system.Book.Entity.Book;
+
+@Component
+public class BookMapper {
+    public Book toEntity(BookRequestDTO requestDTO)
+    {
+
+    }
+
+}
