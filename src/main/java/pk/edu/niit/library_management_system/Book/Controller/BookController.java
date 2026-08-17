5e8c1b437aef3d0de1cb563fdfcc646fbf0503c0
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-08-17 11:40:29 +0500

    refactor: Refactored code in Book Controller

--- src/main/java/pk/edu/niit/library_management_system/Book/Controller/BookController.java
+++ src/main/java/pk/edu/niit/library_management_system/Book/Controller/BookController.java
@@ -11,6 +11,7 @@ import pk.edu.niit.library_management_system.Author.Services.AuthorServices;
 import pk.edu.niit.library_management_system.Book.DTO.BookRequestDTO;
 import pk.edu.niit.library_management_system.Book.DTO.BookResponseDTO;
 import pk.edu.niit.library_management_system.Book.Entity.Book;
+import pk.edu.niit.library_management_system.Book.Mapper.BookMapper;
 import pk.edu.niit.library_management_system.Book.Services.BookServices;
 import pk.edu.niit.library_management_system.ExceptionHandler.BookNotFoundException;
 
@@ -23,7 +24,7 @@ public class BookController {
     @Autowired
     private BookServices bookServices;
     @Autowired
-    private AuthorServices authorServices;
+    private BookMapper bookMapper;
 
     @GetMapping
     public ResponseEntity<List<BookResponseDTO>> getAllBooks()
@@ -34,39 +35,20 @@ public class BookController {
                 throw new BookNotFoundException("GET/books:Books not found");
             }
             List<BookResponseDTO> responseDTOS=books.stream().map(book->{
-                BookResponseDTO responseDTO=new BookResponseDTO();
-                responseDTO.setBookId(book.getBookId());
-                responseDTO.setIsbn(book.getIsbn());
-                responseDTO.setAuthorName(book.getAuthor().getAuthorName());
-                responseDTO.setTitle(book.getTitle());
-                responseDTO.setTotalCopies(book.getTotalCopies());
-                responseDTO.setAvailableCopies(book.getAvailableCopies());
+                BookResponseDTO responseDTO=bookMapper.toResponseDTO(book);
                 return responseDTO;
             }).toList();
             log.info("GET/book : {} Books found",responseDTOS.size());
             return ResponseEntity.ok(responseDTOS);
-
 
     }
 
     @PostMapping
     public ResponseEntity<BookResponseDTO> createBook(@Valid @RequestBody BookRequestDTO dto)
     {
-            Book book=new Book();
-            Author author=authorServices.getAuthorById(dto.getAuthorId());
-            book.setIsbn(dto.getIsbn());
-            book.setTitle(dto.getTitle());
-            book.setAuthor(author);
-            book.setTotalCopies(dto.getTotalCopies());
-            book.setAvailableCopies(dto.getAvailableCopies());
+            Book book=bookMapper.toEntity(dto);
             Book created=bookServices.createBook(book);
-            BookResponseDTO responseDTO=new BookResponseDTO();
-            responseDTO.setBookId(book.getBookId());
-            responseDTO.setIsbn(book.getIsbn());
-            responseDTO.setAuthorName(book.getAuthor().getAuthorName());
-            responseDTO.setTitle(book.getTitle());
-            responseDTO.setTotalCopies(book.getTotalCopies());
-            responseDTO.setAvailableCopies(book.getAvailableCopies());
+            BookResponseDTO responseDTO=bookMapper.toResponseDTO(created);
             log.info("POST/book : Book created for this id :{}",responseDTO.getBookId());
             return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
 
@@ -76,7 +58,6 @@ public class BookController {
     @DeleteMapping("id/{id}")
     public ResponseEntity<?> deleteBook(@PathVariable long id)
     {
-
             bookServices.deleteBook(id);
             log.info("DELETE/book/id/{} : Book deleted for this id : {}",id);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
@@ -86,37 +67,18 @@ public class BookController {
     @PutMapping("id/{id}")
     public ResponseEntity<BookResponseDTO> updateBook(@PathVariable long id, @Valid @RequestBody BookRequestDTO dto)
     {
-            Book book=new Book();
-            Author author= authorServices.getAuthorById(dto.getAuthorId());
-            book.setAvailableCopies(dto.getAvailableCopies());
-            book.setIsbn(dto.getIsbn());
-            book.setTitle(dto.getTitle());
-            book.setAuthor(author);
-            book.setTotalCopies(dto.getTotalCopies());
+            Book book=bookMapper.toEntity(dto);
             Book updateBook = bookServices.updateBook(id, book);
-            BookResponseDTO responseDTO=new BookResponseDTO();
-            responseDTO.setBookId(book.getBookId());
-            responseDTO.setIsbn(book.getIsbn());
-            responseDTO.setAuthorName(book.getAuthor().getAuthorName());
-            responseDTO.setTitle(book.getTitle());
-            responseDTO.setTotalCopies(book.getTotalCopies());
-            responseDTO.setAvailableCopies(book.getAvailableCopies());
+            BookResponseDTO responseDTO=bookMapper.toResponseDTO(updateBook);
             log.info("PUT/book/id/{}: Book updated",id);
             return ResponseEntity.ok(responseDTO);
-
     }
 
     @GetMapping("id/{id}")
     public ResponseEntity<BookResponseDTO> getBookById(@PathVariable long id)
     {
             Book book=bookServices.getBookByID(id);
-            BookResponseDTO responseDTO=new BookResponseDTO();
-            responseDTO.setBookId(book.getBookId());
-            responseDTO.setIsbn(book.getIsbn());
-            responseDTO.setAuthorName(book.getAuthor().getAuthorName());
-            responseDTO.setTitle(book.getTitle());
-            responseDTO.setTotalCopies(book.getTotalCopies());
-            responseDTO.setAvailableCopies(book.getAvailableCopies());
+            BookResponseDTO responseDTO=bookMapper.toResponseDTO(book);
 
             log.info("GET/id/{}: Book found for this id {}",id);
             return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
--- src/main/java/pk/edu/niit/library_management_system/Book/Mapper/BookMapper.java
+++ src/main/java/pk/edu/niit/library_management_system/Book/Mapper/BookMapper.java
@@ -1,14 +1,38 @@
 package pk.edu.niit.library_management_system.Book.Mapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pk.edu.niit.library_management_system.Author.Entity.Author;
+import pk.edu.niit.library_management_system.Author.Services.AuthorServices;
 import pk.edu.niit.library_management_system.Book.DTO.BookRequestDTO;
+import pk.edu.niit.library_management_system.Book.DTO.BookResponseDTO;
 import pk.edu.niit.library_management_system.Book.Entity.Book;
 
 @Component
 public class BookMapper {
+    @Autowired
+    AuthorServices authorServices;
     public Book toEntity(BookRequestDTO requestDTO)
     {
+         Book book=new Book();
+        Author author=authorServices.getAuthorById(requestDTO.getAuthorId());
+        book.setIsbn(requestDTO.getIsbn());
+        book.setTitle(requestDTO.getTitle());
+        book.setAuthor(author);
+        book.setTotalCopies(requestDTO.getTotalCopies());
+        book.setAvailableCopies(requestDTO.getAvailableCopies());
+       return book;
+    }
 
+    public BookResponseDTO toResponseDTO(Book book)
+    {
+        BookResponseDTO responseDTO=new BookResponseDTO();
+        responseDTO.setBookId(book.getBookId());
+        responseDTO.setIsbn(book.getIsbn());
+        responseDTO.setAuthorName(book.getAuthor().getAuthorName());
+        responseDTO.setTitle(book.getTitle());
+        responseDTO.setTotalCopies(book.getTotalCopies());
+        responseDTO.setAvailableCopies(book.getAvailableCopies());
     }
 
 }
