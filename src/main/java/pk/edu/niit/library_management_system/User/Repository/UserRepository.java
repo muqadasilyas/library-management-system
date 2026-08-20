2cf10150d40516203746bc1a56557b2e95e1df08
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-08-20 13:18:35 +0500

    feat: Added User repository for Users

--- src/main/java/pk/edu/niit/library_management_system/User/Repository/UserRepository.java
+++ src/main/java/pk/edu/niit/library_management_system/User/Repository/UserRepository.java
@@ -1,4 +1,13 @@
 package pk.edu.niit.library_management_system.User.Repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pk.edu.niit.library_management_system.User.Entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+    Optional<User> findByUsername(String username);
+
 }
