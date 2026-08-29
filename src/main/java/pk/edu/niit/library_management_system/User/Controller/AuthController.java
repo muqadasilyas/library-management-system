022ccdd157b95de2a888c08f1f16c2daeff281ee
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-08-29 20:13:49 +0500

    update: Added validation in Register Request DTO

--- src/main/java/pk/edu/niit/library_management_system/User/Controller/AuthController.java
+++ src/main/java/pk/edu/niit/library_management_system/User/Controller/AuthController.java
@@ -1,10 +1,29 @@
 package pk.edu.niit.library_management_system.User.Controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pk.edu.niit.library_management_system.User.DTO.RegisterRequest;
+import pk.edu.niit.library_management_system.User.Service.AuthService;
+
 @Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest registerRequest)
+    {
+        authService.register(registerRequest);
+        log.info("POST/auth/register: User registered");
+        return ResponseEntity.ok("User registered successfully");
+
+    }
 }
--- src/main/java/pk/edu/niit/library_management_system/User/DTO/RegisterRequest.java
+++ src/main/java/pk/edu/niit/library_management_system/User/DTO/RegisterRequest.java
@@ -1,9 +1,15 @@
 package pk.edu.niit.library_management_system.User.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @NotBlank(message = "Username must not be blank")
+    @Size(min = 6,max = 18,message =" Size of username should be between 6 and 12" )
     private String userName;
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 6,max = 20,message =" Size of password should be between 6 and 12" )
     private String password;
 }
