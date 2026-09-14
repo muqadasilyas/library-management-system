490cb231ca4729a6768dace5ea324242e1db0a90
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-09-14 09:49:36 +0500

    feat: Added authorization rule in Security config

--- src/main/java/pk/edu/niit/library_management_system/Config/SecurityConfig.java
+++ src/main/java/pk/edu/niit/library_management_system/Config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class);
--- src/main/java/pk/edu/niit/library_management_system/User/Util/Role.java
+++ src/main/java/pk/edu/niit/library_management_system/User/Util/Role.java
@@ -2,6 +2,6 @@ package pk.edu.niit.library_management_system.User.Util;
 
 public enum Role {
     ADMIN,
-    LIBRARIAN,
+
     MEMBER
 }
